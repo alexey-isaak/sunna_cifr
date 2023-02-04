f930bf28ab2e8651ff59f30c35b6d2e59ef4881e
Alexey_I <gafar@xaker.ru>
2023-02-04 17:05:28 +0300

    Itogovy sxript

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+ 
+namespace summa_cifr
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число: ");
+            string chisloN = Convert.ToString(Console.ReadLine());
+            int chisloN_l = chisloN.Length;
+            int i = 0;
+            double sum = 0;
+            while ( i < chisloN_l )
+            {
+            char[] N = chisloN.ToCharArray();
+            int cifra = (int)Char.GetNumericValue(N[i]);
+            sum = sum + cifra;
+            i = i + 1;
+            }
+            Console.WriteLine("Сумма цифр числа: " + sum);
+        }
+    }
+}
+
